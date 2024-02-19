62cdb43c34831429b02e6be817fa84c51a52b9ff
romanhalaichuk <vi2virbien@gmail.com>
2024-02-19 01:40:29 +0100

    kjb

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,17 +6,13 @@ import ContactList from './ContactList/ContactList';
 import styles from './App.module.css';
 
 const App = () => {
-  const deleteContact = id => {
-    console.log('Usuwanie kontaktu o id:', id);
-  };
-
   return (
     <Provider store={store}>
       <div className={styles.phonebookContainer}>
         <h1 className={styles.title}>Phonebook</h1>
         <ContactForm />
         <h2 className={styles.title}>Contacts</h2>
-        <ContactList deleteContact={deleteContact} />{' '}
+        <ContactList />
       </div>
     </Provider>
   );
